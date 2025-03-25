5e59b36554ba83ffb3935adf9b203cf6f297a5c7
makombelajob <50692272+makombelajob@users.noreply.github.com>
2025-03-25 15:21:19 +0100

    comment add

--- js/Components/Burger.js
+++ js/Components/Burger.js
@@ -1,17 +1,29 @@
+// Définition de la fonction Burger
 export function Burger() {
+	// Sélection du bouton burger dans le DOM
 	const burger = document.querySelector("#burger");
 	
+	// Sélection du menu déroulant (élément à afficher/masquer)
 	const openBtn = document.querySelector(".open");
+	
+	// Sélection du bouton de fermeture du menu
 	const closeBtn = document.querySelector("#close");
 	
-
+	// Écouteur d'événements pour l'ouverture du menu lorsque l'utilisateur clique sur le bouton burger
 	burger.addEventListener("click", function (e) {
-		e.stopPropagation();
+		// Empêche la propagation de l'événement (clic sur l'élément parent)
+		//e.stopPropagation();
+		
+		// Affiche le menu déroulant
 		openBtn.style.display = "block";
 	});
 	
+	// Écouteur d'événements pour la fermeture du menu lorsque l'utilisateur clique sur le bouton de fermeture
 	closeBtn.addEventListener("click", function (e) {
-		e.stopPropagation();
+		// Empêche la propagation de l'événement (clic sur l'élément parent)
+		//e.stopPropagation();
+		
+		// Masque le menu déroulant
 		openBtn.style.display = "none";
 	});
 }

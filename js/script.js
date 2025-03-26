3477551b91b19566e3ba9a98cfa5da1813135be3
makombelajob <50692272+makombelajob@users.noreply.github.com>
2025-03-26 11:34:58 +0100

    modification en script js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,41 @@
 import {Burger} from  './Components/Burger.js';
 
-document.addEventListener("DOMContentLoaded", () => {
-	Burger();
+// Chargement des états sauvegardés depuis le localStorage (si disponibles)
+let stockFavoris = JSON.parse(localStorage.getItem('favFood')) || {};
+
+Burger();
+
+const favIcons = document.querySelectorAll(".fav-btn");
+
+for(const favIcon of favIcons) {
 	
-});
+	// Récupération de l'ID unique du dataset
+	const favIconId = favIcon.dataset.favorisId;
+	
+	// Vérification de l'état du bouton est rouge dans l'objet JSON
+	if(stockFavoris[favIconId] === true) {
+		favIcon.style.color = "red";
+	}
+	
+	// Ajout un écouteur d'événement pour chaque bouton
+	favIcon.addEventListener("click", function() {
+		// Vérification de l'état actuel du button, rouge ou pas
+		if(favIcon.style.color === "red") {
+			// Garder la couleur rouge
+			favIcon.style.color = "initial";
+			// Supprimer l'état du button dans stock
+			delete stockFavoris[favIconId];
+		}else{
+			// passer l'état du button en rouge
+			favIcon.style.color = "red";
+			// stocker l'id du button et son état
+			stockFavoris[favIconId] = true;
+		}
+		// Persistance avec LocalStorage
+		const favUser = JSON.stringify(stockFavoris);
+		localStorage.setItem('favFood', favUser);
+		
+	});
+}
+
+

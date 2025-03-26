a3b9ef211f9d0cfb3df20812219c081296f454f5
makombelajob <50692272+makombelajob@users.noreply.github.com>
2025-03-26 22:41:22 +0100

    filtre searchBar prêt

--- js/Components/filtreRecherche.js
+++ js/Components/filtreRecherche.js
@@ -1,14 +1,34 @@
 export function filtreRecherche() {
-	
+	const recettes = document.querySelectorAll(".article");
 	const searchBar = document.querySelector("#searchBar");
-	searchBar.addEventListener("change", function() {
-		console.log(this.value);
-	});
 	
 	const form = document.querySelector("#search");
 	form.addEventListener("submit", function(e) {
 		e.preventDefault();
-		console.log("Hello");
+		
+		const searchValue = searchBar.value;
+		if(!searchValue){
+			alert("Vous devez enter un nom pour chercher");
+			return;
+		}
+		
+		let foundFood = false;
+		
+		for(const recette of recettes) {
+			const title = recette.querySelector(".title").textContent.toLowerCase();
+			
+			if(title.includes(searchValue)) {
+				recette.style.display = "block";
+				foundFood = true;
+			}else{
+				recette.style.display = "none";
+				foundFood = false;
+			}
+		}
+		
+		if(!foundFood) {
+			alert("Aucun Répas dans le stock");
+		}
 	});
 	
 }
--- js/script.js
+++ js/script.js
@@ -1,58 +1,7 @@
 import { Burger } from './Components/Burger.js';
 import {filtreRecherche} from "./Components/filtreRecherche.js";
+import {favBtn} from "./Components/favBtn.js";
 // Fonctionnement du burger
 Burger();
 filtreRecherche();
-
-// Chargement des états sauvegardés depuis le localStorage (si disponibles)
-let stockFavoris = JSON.parse(localStorage.getItem("id")) || [];
-
-// Récupération des icônes de favoris
-const favIcons = document.querySelectorAll(".fav-btn");
-
-
-// Boucler sur chaque bouton en particulier
-for (const favIcon of favIcons) {
-	// selection de type
-	const type = favIcon.closest(".article").querySelector(".type");
-	const timer = favIcon.closest(".article").querySelector(".time");
-	const level = favIcon.closest(".article").querySelector(".level");
-	
-	// Récupération de l'ID unique du dataset du button favoris
-	const favIconId = favIcon.dataset.favorisId;
-	const favIconName = favIcon.dataset.name;
-	
-	//Verifier si l'élément est dans les favoris
-	const favorisIds = stockFavoris.map(fav => fav.id);
-	const isFavorisOk = favorisIds.includes(favIconId);
-	
-	// Appliquer la couleur rouge si favori existe
-	favIcon.style.color = isFavorisOk ? "red" : "initial";
-	
-	// écouteur sur chaque Bouton
-	favIcon.addEventListener("click", function() {
-		
-		// Mise à jour le stock
-		const favorisIds = stockFavoris.map(fav => fav.id);
-
-		if(favorisIds.includes(favIconId)){
-			// Supprimer le favori
-			favIcon.style.color = "initial";
-			stockFavoris = stockFavoris.filter(fav => fav.id !== favIconId);
-		}else{
-			favIcon.style.color = "red";
-			stockFavoris.push({
-				id: favIconId ,
-				name: favIconName,
-				type: type.textContent,
-				time: timer.textContent,
-				level: level.textContent
-			});
-		}
-		
-		// stockage dans le localStorage
-		const stockJson = JSON.stringify(stockFavoris);
-		localStorage.setItem("id", stockJson);
-	});
-}
-
+favBtn();

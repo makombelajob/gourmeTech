1b0bc1d506aea0924a95d8f62927946623953e1b
makombelajob <50692272+makombelajob@users.noreply.github.com>
2025-03-27 10:23:00 +0100

    mode sombre en place

---
+++ js/Components/theme.js
@@ -0,0 +1,32 @@
+export function theme(){
+	// Récuperation du button pour permettre le changement du mode
+	const themeBtn = document.querySelector("#theme");
+	
+	//Récupération du body pour changer le mode
+	const body = document.body;
+	
+	// Vérifier si le theme existe dans le localstorage
+	const themeSaved = localStorage.getItem("theme");
+	
+	if(themeSaved) {
+		body.classList.add("dark");
+	}
+	
+	// Ajouter un écouteur d'événement sur le bouton
+	themeBtn.addEventListener("click", () => {
+		body.classList.toggle("dark");
+		
+		const themeTest = body.classList.contains("dark") ? "dark" : "";
+		localStorage.setItem("theme", themeTest);
+	});
+	
+	// Changement de theme par rapport au système d'exploitation
+	window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
+		body.classList.remove("dark");
+		localStorage.theme = "";
+		if(e.matches){
+			body.classList.add("dark");
+			localStorage.theme = "dark";
+		}
+	})
+}
--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
+// L'import du bouton burger qui est une fonction à part
 import { Burger } from './Components/Burger.js';
+
+// L'import du filtrage de la bar de recherche qui est une fonction à part
 import {filtreRecherche} from "./Components/filtreRecherche.js";
+
+// L'import du button favoris sur les articles qui est une fonction à part
 import {favBtn} from "./Components/favBtn.js";
+
+// L'import du bouton theme qui est une fonction à part
+import {theme} from "./Components/theme.js";
+
+
 // Fonctionnement du burger
 Burger();
+
+// Fonctionnement du filtre
 filtreRecherche();
+
+// Fonctionnement du button favoris
 favBtn();
+
+// Fonctionnement du bouton de theme
+theme();
